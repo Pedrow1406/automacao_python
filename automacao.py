79ea088895ef3acb4929b285c7e9063423040daf
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-28 00:33:03 -0300

    Automação Instagram
    Cuidado! Tomei suspensão do insta por causa disso!!!
    Faz login, faz uma pesquisa pelo usuário e segue esse usuário.

--- automacao.py
+++ automacao.py
@@ -17,6 +17,53 @@ service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 
 
+def instagram_acess(username,your_password, text) -> str:
+    user = '#loginForm > div > div:nth-child(1) > div > label > input'
+    password = '#loginForm > div > div:nth-child(2) > div > label > input'
+    join = '#loginForm > div > div:nth-child(3)'
+
+    save_info_no = '#mount_0_0_0v > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div.x9f619.xvbhtw8.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x1qjc9v5.x1oa3qoh.x1qughib > div.x1gryazu.xh8yej3.x10o80wk.x14k21rp.x17snn68.x6osk4m.x1porb0y > section > main > div > div > div > div > div'
+
+    active_noti_no = 'body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._a9-z > button._a9--._ap36._a9_1'
+
+    lupa_search  =  '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/span/div/a/div'
+    barra_search = '//*[@id="mount_0_0_oC"]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/input'
+    first_profile = '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/a[1]/div[1]/div/div/div[2]/div/div'
+
+    follow = '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[1]/div/div[1]/button/div/div'
+
+    options.add_argument(f'user-data-dir={PROFILE_PATH}')
+    options.add_argument(f'--start-maximized')
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get('https://www.instagram.com/')
+    sleep(5)
+
+    user_login = driver.find_element(By.CSS_SELECTOR, user)
+    user_login.click()
+    sleep(.5)
+    user_login.send_keys(username)
+    sleep(.5)
+    password_login = driver.find_element(By.CSS_SELECTOR, password)
+    password_login.click()
+    sleep(.5)
+    password_login.send_keys(your_password)
+    sleep(.5)
+    driver.find_element(By.CSS_SELECTOR, join).click()
+    sleep(10)
+    driver.find_element(By.CSS_SELECTOR, save_info_no).click()
+    sleep(3)
+    driver.find_element(By.CSS_SELECTOR, active_noti_no).click()
+    sleep(1)
+    driver.find_element(By.XPATH, lupa_search).click()
+    sleep(.5)  
+    barra_pesquisa = driver.find_element(By.XPATH, barra_search)
+    barra_pesquisa.click()
+    sleep(.5)
+    barra_pesquisa.send_keys(text)
+    sleep(2)
+    driver.find_element(By.XPATH, first_profile).click()
+    sleep(.5)
+    driver.find_element(By.XPATH, follow).click()
 def book_toscrap():
     options.add_argument(f'--user-data-dir={PROFILE_PATH}')
     driver = webdriver.Chrome(service=service, options=options)
