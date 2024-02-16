6f468a41d6c895b84455c7a2b01a7e96365918ee
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-02-16 18:52:07 -0300

    Criando a pasta do mês caso ela não exista

--- manipulando_files/movendo_files.py
+++ manipulando_files/movendo_files.py
@@ -3,38 +3,31 @@ import os
 dir_vendas = r'C:\Users\apfri\Pictures\Desktop\historico_vends'
 
 def formata_mes(mes, venda, extensao):
-    if mes not in venda:
-        venda = venda.split(' ')
-        for i in range(len(venda)):
-            if mes in venda[i].lower():
-                venda[i] = mes + extensao
-                break
-        venda = ' '.join(venda)
-        return venda
+    venda = venda.split(' ')
+    for i in range(len(venda)):
+        if mes in venda[i].lower():
+            venda[i] = mes + extensao
+            break
+    venda = ' '.join(venda)
     return venda
-    
+
+meses = ['jan', 'fev', 'mar','abr', 'mai', 'jun', 'jul', 'ago', 'set','out', 'nov', 'dez']  
 for venda in os.listdir(dir_vendas):
     mes = None
     nome_completo = os.path.join(dir_vendas, venda)
     if '.' in venda:
         i_ext = venda.index('.')
-        ext = venda[i_ext:]
+        ext = venda[i_ext:]     
         if ext in venda:
-            if 'jan' in venda.lower():
-                mes = 'Jan'
-                venda = formata_mes('jan', venda, ext)
-                print(venda)
-
-            elif 'fev' in venda.lower():
-                mes = 'Fev'
-                venda = formata_mes('fev', venda, ext)
-                print(venda)
-                
-            elif 'mar' in venda.lower(): 
-                mes = 'Mar'
-                venda = formata_mes('mar', venda, ext)
-                print(venda)
-    
+            for m in meses:
+                if m in venda.lower():
+                    mes = m.capitalize()
+                    venda = formata_mes(m, venda, ext)
+                    print(venda)
+                    break
     if mes:
-        nome_completo_novo = os.path.join(dir_vendas, mes, venda)
+        dir_vendas_mes = os.path.join(dir_vendas, mes)
+        if not os.path.exists(dir_vendas_mes): # Verifica se esse diretório não existe
+            os.mkdir(dir_vendas_mes) # Cria um diretório novo caso ele não exista
+        nome_completo_novo = os.path.join(dir_vendas_mes, venda)
         os.rename(nome_completo, nome_completo_novo)
